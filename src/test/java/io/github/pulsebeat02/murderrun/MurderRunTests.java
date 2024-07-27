3ed70e426a690ef7936ade8699bf166c318629ab
brandonli <brandonli2006ma@gmail.com>
2024-07-27 16:38:22 +0800

    updates

--- src/test/java/io/github/pulsebeat02/murderrun/MurderRunTests.java
+++ src/test/java/io/github/pulsebeat02/murderrun/MurderRunTests.java
@@ -6,26 +6,26 @@ import be.seeseemelk.mockbukkit.entity.PlayerMock;
 
 public final class MurderRunTests {
 
-    private final ServerMock server;
-    private final MurderRun plugin;
+  private final ServerMock server;
+  private final MurderRun plugin;
 
-    public static void main(final String[] args){
-        final MurderRunTests tests = new MurderRunTests();
-        tests.test();
-        tests.tearDown();
-    }
+  public static void main(final String[] args) {
+    final MurderRunTests tests = new MurderRunTests();
+    tests.test();
+    tests.tearDown();
+  }
 
-    public MurderRunTests() {
-        this.server = MockBukkit.mock();
-        this.plugin = MockBukkit.load(MurderRun.class);
-    }
+  public MurderRunTests() {
+    this.server = MockBukkit.mock();
+    this.plugin = MockBukkit.load(MurderRun.class);
+  }
 
-    public void tearDown() {
-        MockBukkit.unmock();
-    }
+  public void tearDown() {
+    MockBukkit.unmock();
+  }
 
-    public void test() {
-        final PlayerMock player = this.server.addPlayer();
-        player.performCommand("murder help");
-    }
+  public void test() {
+    final PlayerMock player = this.server.addPlayer();
+    player.performCommand("murder help");
+  }
 }
