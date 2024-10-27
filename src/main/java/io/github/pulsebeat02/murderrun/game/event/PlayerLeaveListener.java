f60e578f689bd64a30ffb951a26ba810c5c3a458
PulseBeat_02 <brandonli2006ma@gmail.com>
2024-10-26 23:25:46 -0700

    Fixed Player Leave Issue

---
+++ src/main/java/io/github/pulsebeat02/murderrun/game/event/PlayerLeaveListener.java
@@ -0,0 +1,33 @@
+package io.github.pulsebeat02.murderrun.game.event;
+
+import io.github.pulsebeat02.murderrun.game.lobby.PreGameManager;
+import io.github.pulsebeat02.murderrun.game.lobby.PreGamePlayerManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Collection;
+
+public final class PlayerLeaveListener implements Listener {
+
+  private final PreGameManager manager;
+
+  public PlayerLeaveListener(final PreGameManager manager) {
+    this.manager = manager;
+  }
+
+  @EventHandler(priority = EventPriority.LOWEST)
+  public void onPlayerQuit(final PlayerQuitEvent event) {
+
+    final Player player = event.getPlayer();
+    final PreGamePlayerManager playerManager = this.manager.getPlayerManager();
+    final Collection<Player> participants = playerManager.getParticipants();
+    if (!participants.contains(player)) {
+      return;
+    }
+
+    playerManager.removeParticipantFromLobby(player);
+  }
+}
