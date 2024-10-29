e0b367aae93ecb397f7eea565bf14f16291d1ecd
PulseBeat_02 <brandonli2006ma@gmail.com>
2024-10-29 12:05:21 -0700

    Fixed Lobby Timer

--- src/main/java/io/github/pulsebeat02/murderrun/utils/InventoryUtils.java
+++ src/main/java/io/github/pulsebeat02/murderrun/utils/InventoryUtils.java
@@ -1,5 +1,6 @@
 package io.github.pulsebeat02.murderrun.utils;
 
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
@@ -7,6 +8,16 @@ public final class InventoryUtils {
 
   private InventoryUtils() {
     throw new UnsupportedOperationException("Utility class cannot be instantiated");
+  }
+
+  public static ItemStack[] getAllSlotsOnScreen(final Player player) {
+    final PlayerInventory inventory = player.getInventory();
+    final ItemStack[] contents = inventory.getContents();
+    final ItemStack cursor = player.getItemOnCursor();
+    final ItemStack[] slots = new ItemStack[contents.length + 1];
+    System.arraycopy(contents, 0, slots, 0, contents.length);
+    slots[slots.length - 1] = cursor;
+    return slots;
   }
 
   public static boolean consumeStack(final PlayerInventory inventory, final ItemStack stack) {
