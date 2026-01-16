01a44ef2e4b61b6ea52c9ec50f03e69b2f3f63ac
Brandon Li <brandonli2006ma@gmail.com>
2026-01-15 18:32:33 -0800

    Lobby Changes

--- src/main/java/me/brandonli/murderrun/game/map/MapSchematicIO.java
+++ src/main/java/me/brandonli/murderrun/game/map/MapSchematicIO.java
@@ -187,8 +187,8 @@ public final class MapSchematicIO {
     MapUtils.enableExtent();
     this.copyCitizensNPCs();
     this.createWorld();
-    this.pasteLobbySchematic();
-    this.pasteArenaSchematic();
+    this.pasteLobbySchematic().join();
+    this.pasteArenaSchematic(); // let it run in background
     this.pasteCitizensNPCs();
   }
 
